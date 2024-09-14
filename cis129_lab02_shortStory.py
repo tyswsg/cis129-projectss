252369e62284ce779064ed6aa4754e970836e6d5
tyswsg <twashington14@mail.pima.edu>
2024-09-13 20:48:44 -0700

    Updated cis129_lab02_shortStory.py made a short story

--- cis129_lab02_shortStory.py
+++ cis129_lab02_shortStory.py
@@ -1,1 +1,3 @@
-('Hello, World!')
+# Author: Tyler Washington
+# My first Python lab in my CIS129 course
+# (Once upon a time there was a little squirell. The squirelle wanted to climb to the top of the tree to retrive the golden nut. Thus the squirell climbed the tree to teh very tippy top bounding up the branches. Alas, the squirell arrived at the golden nut but when he reached for it he slipped and plunged to his tragic death.) 
